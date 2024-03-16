8530759bb3e3c6a98fb7c2a00bf9dc1670018d58
Edson Cândido <edson.c.a.j@hotmail.com>
2024-03-15 22:39:44 -0300

    [fix] input

--- src/components/LoginForm/styles.ts
+++ src/components/LoginForm/styles.ts
@@ -4,3 +4,8 @@ export const Container = styled.div`
     width: 100vw;
     border:1px red solid;
 `; 
+
+export const Card = styled.div`
+    border: 1px #efefef solid;
+    border-radius: 8px;
+`;
