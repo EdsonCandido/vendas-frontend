df43a7138c171a4b4af8dd2c641ad1696e08a5fe
Edson Cândido <edson.c.a.j@hotmail.com>
2024-03-17 22:08:01 -0300

    [style] correct login form

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,9 +1,14 @@
-import * as S from './styles';
+import Input from "../Input";
+import * as S from "./styles";
+//import { LockOutlined, UserOutlined } from "@ant-design/icons";
 const LoginForm = () => {
-    return (
-        <S.Container>
 
-        </S.Container>
-    );
+  return (
+    <S.Container>
+      <S.Card>
+        <Input />
+      </S.Card>
+    </S.Container>
+  );
 };
 export default LoginForm;
--- src/components/LoginForm/styles.ts
+++ src/components/LoginForm/styles.ts
@@ -2,10 +2,28 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100vw;
-    border:1px red solid;
+    /* border:1px red solid; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `; 
 
 export const Card = styled.div`
+width: 22vw;
+height: 40vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
     border: 1px #efefef solid;
-    border-radius: 8px;
+    /* border:1px red solid; */
+    border-radius: 5px;
+    /* height: 15px; */
+    /* width: 15px;; */
+`;
+
+export const ButtonLogin = styled.div`
+    display: flex;
+    justify-content: end;
+    align-items: center;
 `;
