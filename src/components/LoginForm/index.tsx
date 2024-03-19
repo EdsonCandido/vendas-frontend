a33e6b6e816391f9a80ea54c7abb8355f49061f7
Edson Cândido <edson.c.a.j@hotmail.com>
2024-03-18 21:36:31 -0300

    [fix] .

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,12 +1,38 @@
-import Input from "../Input";
+// import Input from "../Input";
+import { Button, Flex, Input } from "antd";
 import * as S from "./styles";
+import React from "react";
 //import { LockOutlined, UserOutlined } from "@ant-design/icons";
 const LoginForm = () => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <S.Container>
       <S.Card>
-        <Input />
+        <form onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="login">Login</label>
+            <Input placeholder="Login" id="login" name="login" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password">Senha</label>
+            <Input
+              placeholder="Senha"
+              id="password"
+              name="password"
+              type="password"
+            />
+          </div>
+
+          <Flex justify="end">
+            <Button type="primary" htmlType="submit">
+              {" "}
+              Entrar{" "}
+            </Button>
+          </Flex>
+        </form>
       </S.Card>
     </S.Container>
   );
